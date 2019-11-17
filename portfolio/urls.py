85e02043dce8a06aeb06cdc4f917e45dcb1ad943
Nick <plus0bit@gmail.com>
2019-11-17 09:29:23 +0300

    Added bootstrap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
